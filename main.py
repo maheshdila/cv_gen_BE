0405612ce02170447eaf08a9ad8c55f1717d66b3
maheshdila <maheshdilshan716@gmail.com>
2025-06-13 19:17:03 +0530

    relative paths added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # === app/main.py ===
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from cv_gen_BE.api.routes import router
+from api.routes import router
 
 app = FastAPI()
 
