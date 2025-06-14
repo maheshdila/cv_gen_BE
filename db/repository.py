1c491de2dce4f9e20f255bc67408b9e7b2f2d0c7
SahanVishwajith <sahanwickramasinghe@cse.mrt.ac.lk>
2025-06-14 19:57:00 +0530

    feat : update user data API fix

--- db/repository.py
+++ db/repository.py
@@ -7,3 +7,15 @@ def get_user_data(email:str):
     response = table.get_item(Key={"email":email.strip()})
     return response.get("Item")
 
+def update_latest_raw_input(email: str, new_raw_input: str,update_time:str) -> dict:
+
+    resp = table.update_item(
+        Key={"email": email},
+        UpdateExpression="SET raw_input = :ri, created_at = :ca",
+        ExpressionAttributeValues={
+            ":ri": new_raw_input,
+            ":ca": update_time,
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return resp.get("Attributes", {})
