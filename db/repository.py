64531228750a358774eafd683dede22ac9a66707
SahanVishwajith <sahanwickramasinghe@cse.mrt.ac.lk>
2025-06-14 18:59:00 +0530

    feat : get user data API fix

--- db/repository.py
+++ db/repository.py
@@ -4,5 +4,5 @@ def save_user_data(email: str,data:dict):
     table.put_item(Item={"email":email, **data})
 
 def get_user_data(email:str):
-    response = table.get_item(Key={"email":email})
+    response = table.get_item(Key={"email":"sahan@email.com"})
     return response.get("Item")
