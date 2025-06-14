42a8e0cff1461d687ccfac4c1df35d6ec9c27ef7
maheshdila <maheshdilshan716@gmail.com>
2025-06-15 00:51:46 +0530

    auth system implemented

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
 # === app/api/routes.py ===
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from models.user import UserQuery
 from services.cv_service import generate_cv_from_user
+from auth.token_verifier_utility import verify_token
 
 router = APIRouter()
 
@@ -10,5 +11,5 @@ async def generate_cv(user_input: UserQuery):
     return await generate_cv_from_user(user_input)
 
 @router.get("/test")
-def api_test():
+def api_test(user=Depends(verify_token)):
     return {"message": "FastAPI server is running..."}
